2d635b6df3dc57fb7e93a82082f03918c6ee6bf3
isaiahaiasi <isaiah.althouse@gmail.com>
2021-02-25 23:29:26 -0700

    Extend save loader to allow loading directly from JSON preset

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@
 import './styles/reset.css';
 import './styles/style.css';
 
-//* LOGIC IMPORTS
-import { taskMock } from './model/task';
-import Project from './model/project';
-import Directory from './model/directory';
-
 //* VIEW IMPORTS
 import DirectoryView from './view/dir_view';
 
@@ -14,24 +9,10 @@ import DirectoryView from './view/dir_view';
 import saveLocal from './save_local';
 import SaveHandler from './save_handler';
 
-const generateDefaultDirectory = () => {
-  console.log('Generating defaults');
-  const testProjects = [
-    new Project({ title: 'Test Project', description: 'My stupid test project' }),
-    new Project({ title: 'Test 2', description: 'My SECOND stupid test project' }),
-    new Project({ title: 'This real project', description: '(jk, it\'s another test)' }),
-  ];
-
-  testProjects.forEach((testProject) => {
-    const tasks = taskMock.getMockTasks(7, testProject);
-    testProject.addChild(...tasks);
-  });
-  return new Directory({ title: 'RootDirectory', children: testProjects });
-};
 // SAVE HANDLING
 const saveHandler = SaveHandler(saveLocal);
 
-const directory = saveHandler.handleLoad('DIRECTORY') ?? generateDefaultDirectory();
+const directory = saveHandler.handleLoad('DIRECTORY') ?? saveLocal.loadDefault();
 
 // POPULATING DOM
 const projViewCntr = document.querySelector('.projects-container');
--- src/save_local.js
+++ src/save_local.js
@@ -2,12 +2,65 @@ import ModelBase from './model/modelBase';
 import Task from './model/task';
 import Project from './model/project';
 import Directory from './model/directory';
+import defaultData from './demo_dir.json';
 
-// save an item in localstorage at its ID's location
-const save = (saveableItem) => {
-  console.log(`saving ${saveableItem.title}`);
-  const dirJSON = JSON.stringify(saveableItem.getSerializable());
-  window.localStorage.setItem(saveableItem.getID(), dirJSON);
+const _getConstructor = (saveableItemLiteral) => {
+  // This is so annoying, because I feel like I'm close to being class-agnostic
+  switch (saveableItemLiteral.Type) {
+    case 'Task':
+      return Task;
+    case 'Project':
+      return Project;
+    case 'Directory':
+      return Directory;
+    default:
+      console.log(`Did not find valid type for ${saveableItemLiteral.title}`);
+      return ModelBase;
+  }
+};
+
+const _assignNestedObj = (saveableItem) => {
+  if (!saveableItem || !saveableItem.children) {
+    console.error('Tried to load invalid object.');
+    return null;
+  }
+
+  const children = saveableItem.children.map((child) => _assignNestedObj(child));
+  const Constructor = _getConstructor(saveableItem);
+
+  // I feel like using the spread operator to override properties
+  //  PROBABLY isn't best practice, but idk... seems okay?
+  return new Constructor({ ...saveableItem, children });
+};
+
+const _loadNestedObj = (ID) => {
+  const item = JSON.parse(window.localStorage.getItem(ID));
+
+  if (!item) {
+    console.warn(`Could not find localStorage data for ${ID}!`);
+    return null;
+  }
+
+  const children = item.children.length > 0
+    ? item.children.map((childID) => _loadNestedObj(childID))
+    : [];
+
+  return { ...item, children };
+};
+
+const load = (localStorageKey) => {
+  const nestedObj = _loadNestedObj(localStorageKey);
+
+  if (!nestedObj) {
+    return null;
+  }
+
+  return _assignNestedObj(nestedObj);
+};
+
+const loadDefault = () => {
+  console.log(defaultData);
+  return _assignNestedObj(defaultData);
 };
 
 // remove a localStorage key (ie, when the referenced obj is deleted)
@@ -17,45 +70,15 @@ const remove = (saveableItem) => {
   window.localStorage.removeItem(saveableItem.getID());
 };
 
-// return the saved array of projects from localStorage
-const load = (localStorageKey) => {
-  console.log('Attempting to load from localStorage');
-
-  const itemJSON = window.localStorage.getItem(localStorageKey);
-  const itemLiteral = JSON.parse(itemJSON);
-
-  if (!itemLiteral) {
-    console.log(`Could not find localStorage data for ${localStorageKey}!`);
-    return null;
-  }
-
-  console.log(`loading ${itemLiteral.title}`);
-
-  // recursively load all children
-  const children = itemLiteral.children.map((childID) => load(childID));
-
-  itemLiteral.children = children;
-
-  // This is so annoying, because I feel like I'm close to being class-agnostic
-  let ItemConstructor;
-  switch (itemLiteral.Type) {
-    case 'Task':
-      ItemConstructor = Task;
-      break;
-    case 'Project':
-      ItemConstructor = Project;
-      break;
-    case 'Directory':
-      ItemConstructor = Directory;
-      break;
-    default:
-      console.log(`Did not find valid type for ${itemLiteral.title}`);
-      ItemConstructor = ModelBase;
-      break;
-  }
-  return new ItemConstructor(itemLiteral);
+// save an item in localstorage at its ID's location
+const save = (saveableItem) => {
+  console.log(`saving ${saveableItem.title}`);
+  const dirJSON = JSON.stringify(saveableItem.getSerializable());
+  window.localStorage.setItem(saveableItem.getID(), dirJSON);
 };
 
-const saveLocal = { save, load, remove };
+const saveLocal = {
+  save, load, loadDefault, remove,
+};
 
 export default saveLocal;
